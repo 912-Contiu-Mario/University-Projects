22b20a9b4693bba7b3c6546c9186ac0ec538e00c
912-Contiu-Mario <mario.contiu@stud.ubbcluj.ro>
2024-02-28 14:15:58 +0200

    A

--- LocalMovieDB/Utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-
-std::vector<std::string> tokenize(std::string& str, char delimiter);
-std::string trim(std::string& to_trim);
-std::string r_redundant_spaces(std::string& to_process);
-
-void test_trim();
--- LocalMovieDB/WatchList.cpp
+++
@@ -1,4 +0,0 @@
-#include "WatchList.h"
-
-WatchList::WatchList(std::string filename, std::string user_data_filename): Repository(user_data_filename), filename{filename}
-{}
